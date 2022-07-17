55d8155b834728c15a3a4dfe8ad5db5125a554de
renatoalencar <alencar.renato@yahoo.com.br>
2022-07-17 16:30:45 -0300

    Varios consumdores, produtores e LOG ouvindo os Topicos.

---
+++ src/main/java/br/com/alura/LogService.java
@@ -0,0 +1,62 @@
+package br.com.alura;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+import java.util.regex.Pattern;
+
+public class LogService {
+    public static void main(String[] args) {
+        var consumer = new KafkaConsumer<String, String>(properties());
+
+        //Consumir qualquer topico qe comece com ECOMMERCE.
+        consumer.subscribe(Pattern.compile("ECOMMERCE.*"));
+
+        // Buscar as mensaggens no bloco while(true), até encontrar
+        while (true) {
+
+            //Verificar se existe mensagem por um periodo dias/tempo
+            var records = consumer.poll(Duration.ofMillis(100));
+
+            //validação de registros encontrados e a quantidade.
+
+            if (!records.isEmpty()) {
+                System.out.println(" Encontrei "+records.count() + " registros");
+
+                for (var record : records) {
+                    System.out.println("-----");
+                    System.out.println("LOG"+ record.topic());
+                    System.out.println("key: "+record.key());
+                    System.out.println("Value: "+record.value());
+                    System.out.println("partition: "+record.partition());
+                    System.out.println("offset: "+record.offset());
+
+                }
+            }
+
+        }
+    }
+
+    private static Properties properties() {
+        var properties = new Properties();
+
+        //dentro do bloco while(truue) para rodar ate encontrar as mensagens
+
+            //Onde está rodando o Kafka - ex:localhost:9092
+            properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+
+            //Deserializado de byte para String
+            properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+            properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+
+            //Corrção InvalidropExceptions
+            properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, LogService.class.getSimpleName());
+
+
+            return properties;
+        }
+}
--- src/main/java/br/com/alura/NewOrderMain.java
+++ src/main/java/br/com/alura/NewOrderMain.java
@@ -1,5 +1,6 @@
 package br.com.alura;
 
+import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
@@ -10,25 +11,33 @@ import java.util.Properties;
 import java.util.concurrent.ExecutionException;
 
 public class NewOrderMain {
+
+    private static Callback callback;
+
     public static void main(String[] args) throws ExecutionException, InterruptedException {
 
         //Enviar/Produzir uma mensagem no Kafka
         var producer = new KafkaProducer<String, String>(properties());
 
         //Dados serão enviados para a Fila
-        var value = "123123, 67523, 38495847";
+        var value = "384738, 38495, 930495";
 
         //Enviar os dados para nosso Topic.
         var record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", value, value);
 
         //Retornar mensaegm ou CallBack
-        producer.send(record, (data, exception) -> {
+        callback = (data, exception) -> {
             if (exception != null) {
                 exception.printStackTrace();
                 return;
             }
             System.out.println("Sucesso Enviando nesse topico: " + data.topic() + ":::partition " + data.partition() + "/ offset " + data.offset() + "/ timestamp " + data.timestamp());
-        }).get();
+        };
+        producer.send(record, callback).get();
+
+        var email = "Thank you for your order! We are processing your order!";
+        var emailRecord = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL", email, email);
+        producer.send(emailRecord, callback).get();
     }
 
     private static Properties properties() {
