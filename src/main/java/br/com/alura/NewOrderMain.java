d92e00900dcffd0bb7f5ab4392bca63d622ae0de
renatoalencar <alencar.renato@yahoo.com.br>
2022-07-17 23:58:06 -0300

    criando uma classe Order e implementando Gson serializar

--- src/main/java/br/com/alura/NewOrderMain.java
+++ src/main/java/br/com/alura/NewOrderMain.java
@@ -1,5 +1,6 @@
 package br.com.alura;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
@@ -7,20 +8,28 @@ public class NewOrderMain {
 
     public static void main(String[] args) throws ExecutionException, InterruptedException {
 
-        try (var dispatcher = new KafkaDispatcher()) {
-            //Teste enviar mensagem 100x - obs: numero de particões maior ou igual numero de consumidores detro de um grupo.
-            for (var i = 0; i < 10; i++) {
+        try (var orderDispatcher = new KafkaDispatcher<Order>()) {
+            try (var emailDispatcher = new KafkaDispatcher<String>()) {
 
-                //ver como trabalhar em particoes diferentes, precisou criar UUID para não ir o mesmo dados
-                var key = UUID.randomUUID().toString();
-                //Dados serão enviados para a Fila
-                var value = key + " 0349, 30495 ";
 
-                //Enviar os dados para nosso Topic.
-                dispatcher.send("ECOMMERCE_NEW_ORDER", key, value);
+                //Teste enviar mensagem 10x - obs: numero de particões maior ou igual numero de consumidores detro de um grupo.
+                for (var i = 0; i < 10; i++) {
 
-                var email = "Thank you for your order! We are processing your order!";
-                dispatcher.send("ECOMMERCE_SEND_EMAIL", key, value);
+                    //Dados serão enviados para a Fila
+                    //ver como trabalhar em particoes diferentes, precisou criar UUID para não ir o mesmo dados
+                    //Criando uma Order
+                    var userId = UUID.randomUUID().toString();
+                    var orderId = UUID.randomUUID().toString();
+                    var amount = new BigDecimal(Math.random() * 5000 + 1);
+
+                    var order = new Order(userId, orderId, amount);
+
+                    //Enviar dados para os Topicos abaixo;
+                    orderDispatcher.send("ECOMMERCE_NEW_ORDER", userId, order);
+
+                    var email = "Thank you for your order! We are processing your order!";
+                    emailDispatcher.send("ECOMMERCE_SEND_EMAIL", userId, email);
+                }
             }
         }
     }
